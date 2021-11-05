6282611e6cf88a33fbfa118ca7e3fb732c13624e
Sergey <Sergey@pingvin.club>
2021-11-05 18:56:01 +0300

    Removed temp variable

--- main.cpp
+++ main.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 
 template <typename T, typename F>
-void BubleSort (int *arr, int size)
+void BubbleSort (int *arr, int size)
 {
     F temp;
-    F temp2;
     while (size)
     {
         for (int i = 0; i < size-1; ++i) {
             if (arr[i] > arr[i + 1]) {
                 temp = arr[i];
-                temp2 = arr[i+1];
+                arr[i] = arr[i+1];
                 arr[i+1] = temp;
-                arr[i] = temp2;
             }
         }
         --size;
@@ -23,7 +21,7 @@ void BubleSort (int *arr, int size)
 int main() {
     const int size = 10;
     int *arr = new int [size]{4,2,1,8,0,5,3,9,7,6,};
-    BubleSort <int,int> (arr,size);
+    BubbleSort <int,int> (arr,size);
     for (int i = 0; i < size; ++i) {
         std::cout << arr[i] << std::endl;
     }
